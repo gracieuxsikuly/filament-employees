5307dc21fbb8b5413ac60e6f6d2f7c703525e0b4
gracieuxsikuly <graciersikuly@gmail.com>
2024-06-24 12:07:02 +0200

    good

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Filament\Resources\EmployeeResource;
+use App\Models\Employee;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::get('/employees', function () {
+        $employees=Employee::orderBy('last_name')->get();
+    return EmployeeResource::collection($employees);
+});
