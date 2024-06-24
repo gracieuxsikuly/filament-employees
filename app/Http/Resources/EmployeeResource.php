c522fc4b22326e803bb7468fd5a2afa37d4763ca
gracieuxsikuly <graciersikuly@gmail.com>
2024-06-24 11:28:24 +0200

    good

--- app/Http/Resources/EmployeeResource.php
+++ app/Http/Resources/EmployeeResource.php
@@ -15,7 +15,22 @@ class EmployeeResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-
+            'id'=> $this->id,
+            'first_name'=> $this->first_name,
+            'last_name'=> $this->last_name,
+            'country'=> $this->country->name,
+            'stateId'=> $this->state_id,
+            'cityId'=> $this->city_id,
+            'departmentId'=> $this->department_id,
+            'birth_date'=> $this->birth_date,
+            'hire_date'=> $this->hire_date,
+            'address'=> $this->address,
+            'zipcode'=> $this->zipcode,
         ];
+
+    //     protected $fillable = ['first_name',
+    //     'last_name', 'city_id','state_id',
+    //     'department_id', 'country_id',
+    //    'birth_date', 'hire_date', 'address', 'zipcode','avatar'];
     }
 }
